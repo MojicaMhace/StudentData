32ed06864813c923c25e085cef143fd78485857d
MhaceMojica <mhacegwyneth04@gmail.com>
2024-10-19 02:18:30 +0800

    daminamangnangyayari sayo.

--- StudentCredetialsDL/obj/Debug/netstandard2.0/GradeManagementDL.AssemblyInfo.cs
+++ StudentCredetialsDL/obj/Debug/netstandard2.0/GradeManagementDL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GradeManagementDL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6ecde23d3cd0ddd580a5fb1c8b502b11a66c873")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16f299f3b9c417bef5350923bc907b27a2cbe301")]
 [assembly: System.Reflection.AssemblyProductAttribute("GradeManagementDL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GradeManagementDL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
